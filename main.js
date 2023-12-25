00978d44939e25f26e2aec9d5f72ca56705cd1b6
Sutharsan <132842657+Sudar2002@users.noreply.github.com>
2023-12-25 16:44:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const path =require('path')
 const logInRouter=require('./routes/log_in');
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded());
